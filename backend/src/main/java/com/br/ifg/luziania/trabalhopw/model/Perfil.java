7a569007a5b189c4aefc2ae90d2691b611fb4746
João Gabriel <joaobiel2004@gmail.com>
2026-09-13 20:53:24 -0300

    ajustes

---
+++ backend/src/main/java/com/br/ifg/luziania/trabalhopw/model/Perfil.java
@@ -0,0 +1,38 @@
+package com.br.ifg.luziania.trabalhopw.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "perfis")
+public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String nome;
+
+    @Column(length = 150)
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
